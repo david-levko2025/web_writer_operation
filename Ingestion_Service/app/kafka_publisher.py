a06cc36d7478f9b389bac3f19a511e42bee8da17
david <mdavidl@gmail.com>
2026-02-26 11:32:34 +0200

    cosmetics changes

--- Ingestion_Service/app/kafka_publisher.py
+++ Ingestion_Service/app/kafka_publisher.py
@@ -1,10 +1,11 @@
 from confluent_kafka import Producer
 import json
 
+from ingestion_config import config
 class KafkaPublisher:
-    def __init__(self, servers):
-        self.producer = Producer({'bootstrap.servers': servers})
+    def __init__(self):
+        self.producer = Producer({'bootstrap.servers': config.kafka_servers})
 
-    def publish_raw_event(self, topic, data):
-        self.producer.produce(topic, json.dumps(data).encode('utf-8'))
+    def publish_raw_event(self,data):
+        self.producer.produce(topic=config.kafka_topic ,value= json.dumps(data).encode('utf-8'))
         self.producer.flush()
